e7971993cc3776ec34b90f755bf65171f6f2dccd
Micah Sterling <msterling89@gmail.com>
2022-07-01 12:54:46 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Agenda from './agenda'
+
+function App() {
+  return (
+    <div className="App">
+      <Agenda />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/agenda/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Agenda() {
+
+  return (
+    <div>
+      <h1>Meeting Agenda</h1>
+
+    </div>
+  )
+}
+
+export default Agenda;
